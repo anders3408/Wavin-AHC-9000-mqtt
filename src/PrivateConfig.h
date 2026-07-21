598ecfa025d6e4c13e2c82bc762dcd3efb4420a5
Anders Nielsen <ann@agillix.dk>
2026-07-21 12:57:05 +0200

    Replace PrivateConfig.h with personal information

--- src/PrivateConfig.h
+++ src/PrivateConfig.h
@@ -1,12 +1,12 @@
 #include <ESP8266WiFi.h>
 
-const String   WIFI_SSID = "Enter wireless SSID here";         // wifi ssid
-const String   WIFI_PASS = "Enter wireless password here";     // wifi password
+const String   WIFI_SSID = "UniFi";         // wifi ssid
+const String   WIFI_PASS = "berkohund";     // wifi password
 
-const String   MQTT_SERVER = "Enter mqtt server address here"; // mqtt server address without port number
-const String   MQTT_USER   = "Enter mqtt username here";       // mqtt user. Use "" for no username
-const String   MQTT_PASS   = "Enter mqtt password here";       // mqtt password. Use "" for no password
-const uint16_t MQTT_PORT   = 1883;                             // mqtt port
+const String   MQTT_SERVER = "192.168.2.2"; // mqtt server address without port number
+const String   MQTT_USER   = "";       // mqtt user. Use "" for no username
+const String   MQTT_PASS   = "";       // mqtt password. Use "" for no password
+const uint16_t MQTT_PORT   = 49154;                             // mqtt port
 
 const float    MIN_TEMP    = 6.0;                              // minimum temperature to set
 const float    MAX_TEMP    = 40.0;                             // maximum temperature to set
